19beebe2cd37d1381b664523cec00b900fee0286
cisse17 <119404406+cisse17@users.noreply.github.com>
2026-09-08 23:38:47 +0200

    Fix production static asset paths

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,17 +3,18 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", //en local
+  // base: "/", //en local
   // base: process.env.NODE_ENV === "production" ? "/static/" : "/",
 
-  // base: "/static/", //  Pour que Django trouve les fichiers en prod
+  base: "/static/", //  Pour que Django trouve les fichiers en prod
   build: {
     // outDir: "backend/frontend_build",
-    outDir: "frontend_build",
+    // outDir: "frontend_build",
+    outDir: "../backend/frontend_build",
   },
 
-  // Tu n’as pas besoin du server.proxy en production car React est précompilé. Donc tu peux rendre la section server optionnelle.
-  // Mais si tu veux que ça reste clair etfonctionne en local et en prod avec Docker,
+  // On a pas besoin du server.proxy en production car React est précompilé. Donc on peux rendre la section server optionnelle.
+  // Mais si on veux que ça reste clair etfonctionne en local et en prod avec Docker,
   server: {
     port: 3000,
     // host: true,
